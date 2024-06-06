d878fb4b75dbdf00070d4abff27e544212aa3d4d
conor-murphy <conor.murphy@harvardbusiness.org>
2024-06-06 13:20:53 +0100

    start of section:react capabilities

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,16 +1,35 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import Results from './Results';
+import useBreedList from './useBreedList';
 const ANIMAL = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-  const breeds = [];
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
+
+  useEffect(() => {
+    requestPets();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={e => {
+        e.preventDefault();
+        requestPets();
+      }}>
         <lable htmlFor="location">
           Location
           <input onChange={(e) => setLocation(e.target.value)} id="location" value={location} placeholder="Location" />
@@ -49,6 +68,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
